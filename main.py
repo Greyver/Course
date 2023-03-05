d23c6c5bd3e94fdee1aee3b8efff2b97200213b1
Denis <shapranovdenis@gmail.com>
2023-03-05 08:44:59 +0200

    start place for program

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask("__name__")
@@ -7,7 +7,7 @@ app = Flask("__name__")
 
 @app.route("/")
 def index():
-    return "index"
+    return render_template("index.html", title="Пробник")
 
 
 @app.route("/about")
@@ -16,13 +16,14 @@ def about():
 
 
 DOL_GRN = 'https://www.google.com/search?q=dollar+to+uah&oq=Doolar+to+u&aqs=chrome.1.69i57j0i10i512l9.14327j1j9&sourceid=chrome&ie=UTF-8'
+# получается при помощи my User Agent, защита от ботов
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
 full_page = requests.get(DOL_GRN, headers=headers)
 
 soup = BeautifulSoup(full_page.content, 'html.parser')
-
+# выборка нужного тега и информации
 convert = soup.find(
     "span", {"class": "DFlfde SwHCTb", "data-precision": "2"})
 print(convert.text.replace('.', ','))
